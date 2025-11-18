2abbbfa4ee3a6114f3689dc6013ebcf7dbe3fd58
biztinct <30279090+biztinct@users.noreply.github.com>
2025-11-19 07:11:16 +1100

    .

--- addons/health_invoicing/__manifest__.py
+++ addons/health_invoicing/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'VAFHS Healthcare Invoicing Integration',
-    'version': '18.0.1.0.20',
+    'version': '18.0.1.0.21',
     'category': 'Healthcare/Accounting',
     'summary': 'Healthcare invoicing integration inheriting from standard Odoo accounting',
     'description': """
